8f253f923b5d9d95158632268f32b94678c27d7c
Maks Verver <maksverver@geocities.com>
2009-11-15 19:53:21 +0100

    collect-results:  - Display player failures visually  - Group results per 80 rows so they can be placed in separate posts.

--- collect-results.py
+++ collect-results.py
@@ -27,14 +27,16 @@ def print_rondes_head():
                      for i in [1,2,3,4] ])
     print('[tr]' + th('Ronde') + line + th('Details') + '[/]')
 
-def print_ronde(i, path, scores):
+def print_ronde(ronde, path, scores):
     html = '[url=' + url_prefix + path.replace('.xml', '.html') + ']html[/]'
     xml  = '[url=' + url_prefix + path + ']xml[/]'
-    colors = ['#eee', '#ddd', '#eee', '#ddd']
-    line = ''.join([ td([naam, b(naam)][score == 0], color=col) +
-                     td(score, color=col, align='right')
-                     for col, (naam, score) in zip(colors, scores) ])
-    print('[tr]' + td(i, align='center') + line + td(html + ', ' + xml) + '[/]')
+    line = ''
+    for i, (naam, score, failed) in enumerate(scores):
+        col = ['#eee', '#ddd', '#eee', '#ddd'][i]
+        if score == 0: naam = b(naam)
+        if failed: naam = '[red]' + naam + '[/red]'
+        line += td(naam, color=col) + td(score, color=col, align='right')
+    print('[tr]' + td(ronde, align='center') + line + td(html + ', ' + xml) + '[/]')
 
 def print_rondes_tail():
     print('[tr]' + th(' ', colspan=10) + '[/tr]')
@@ -44,7 +46,7 @@ def print_totaal_head():
     print('[br]')
     # print('[h2]Totaalscores[/h2]')
     print('[table]')
-    print('[tr]' + th('Plaats') + th('Deelnemer') + th('Score') + '[/]')
+    print('[tr]' + th('Plaats') + th('Deelnemer') + th('Cumulatieve score') + '[/]')
 
 def print_totaal(i, name, score):
     print('[tr]' + td(i, align='center') + td(name) + td(score, align='right') + '[/]')
@@ -55,23 +57,36 @@ def print_totaal_tail():
 def get_scores(path):
     doc = ET.parse(path)
     names  = {}
+    failed = {}
     for player in doc.findall('setup/players/player'):
         names[int(player.attrib['id'])] = player.find('name').text
+    for turn in doc.findall('turns/turn'):
+        if turn.find('error') is not None:
+            failed[int(turn.attrib['player'])] = True
     scores = {}
     for score in doc.findall('scores/score'):
         scores[int(score.attrib['player'])] = int(score.text)
-    return [ (names[id], scores[id]) for id in [1,2,3,4] ]
+    return [ (names[id], scores[id], failed.get(id, False))
+             for id in [1,2,3,4] ]
 
 def collect(dir):
     total = {}
 
     print_rondes_head()
-    for i, path in enumerate(glob.glob(os.path.join(dir, '*.xml'))):
+    files = glob.glob(os.path.join(dir, '*.xml'))
+    files.sort()
+    for i, path in enumerate(files):
+        if i > 0 and i%80 == 0:
+            print_rondes_tail()
+            print_rondes_head()
+
         scores = get_scores(path)
         print_ronde(i + 1, os.path.basename(path), scores)
-        for name, score in scores:
+        min_score = 0
+        # min_score = min([ score for _, score, _ in scores ])
+        for name, score, _ in scores:
             if name not in total: total[name] = 0
-            total[name] += score
+            total[name] += score - min_score
     print_rondes_tail()
 
     print_totaal_head()
