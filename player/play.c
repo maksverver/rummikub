d0a4bd50d934e04ea5a700fa3de2beb1f950e358
Maks Verver <maksverver@geocities.com>
2009-11-22 15:20:31 +0100

    player:  - implemented holding back sets on rack whenever this is possible without    lowering the total playable score. Testing suggests this does not actually improve game play. :-/ Must determine if this is worthwhile before the final competition.

--- player/play.c
+++ player/play.c
@@ -1,17 +1,80 @@
 #include "player.h"
 
-/* "This does everything, and consequently is very simple." (A.P.) */
-
-Set *play(const GameState *gs)
+/* Removes a set from a tile collection, and returns its total value. */
+static int reserve_set(TileCollection tiles)
 {
-    Set *new_table = NULL;
-    int new_value = max_value(gs, &new_table);
+    int v, c;
 
-    if (new_value == collection_value(gs->table))
+    /* Search for a group: */
+    for (v = 0; v < V; ++v)
     {
-        free_table(new_table);
-        new_table = NULL;
+        int cnt = 0;
+        for (c = 0; c < C; ++c) if (tiles[v][c] > 0) ++cnt;
+        if (cnt >= 3)
+        {
+            /* Found a group! */
+            for (c = 0; c < C; ++c) if (tiles[v][c] > 0) --tiles[v][c];
+            return cnt*(v + 1);
+        }
     }
 
-    return new_table;
+    /* Search for a run: */
+    for (c = 0; c < C; ++c)
+    {
+        for (v = 0; v < V; ++v)
+        {
+            int len = 0;
+            while (v + len < V && tiles[v + len][c] > 0) ++len;
+            if (len >= 3)
+            {
+                /* Found a run! */
+                int w;
+                for (w = v; w < v + len; ++w) --tiles[w][c];
+                return len*(2*v + len + 1)/2;
+            }
+        }
+    }
+
+    return 0;
+}
+
+/* "This does everything, and consequently is very simple." (A.P.) */
+Set *play(const GameState *gs)
+{
+    int table_value = collection_value(gs->table);
+    int total_value = table_value + collection_value(gs->tiles);
+    Set *best_result;
+    int best_value = max_value(gs, &best_result);
+
+    assert(best_value >= table_value && best_value <= total_value);
+    if (best_value == table_value)
+    {
+        /* Can't play anything now -- just draw */
+        free_table(best_result);
+        return NULL;
+    }
+
+    if (best_value < total_value)  /* table_value < value < total_value */
+    {
+        /* Try to hold back a combination: */
+        GameState new_gs = *gs;
+        int reserved_value = reserve_set(new_gs.tiles);
+        if (reserved_value > 0)
+        {
+            Set *new_result;
+            int new_value;
+
+            /* Check if this is just as good as playing all we can: */
+            new_value = max_value(&new_gs, &new_result);
+            assert(new_value + reserved_value <= best_value);
+            if ( new_value > table_value &&
+                 new_value + reserved_value == best_value )
+            {
+                free_table(best_result);
+                best_result = new_result;
+            }
+        }
+    }
+
+    return best_result;
 }
